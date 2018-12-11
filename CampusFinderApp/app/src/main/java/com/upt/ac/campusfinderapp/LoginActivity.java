7baa9463f7d14af50b92933b123145fd3e659a22
AlexandraAndritoi <alexandraandritoi1@gmail.com>
2018-12-11 17:55:16 +0200

    Menu and Outdoor Map activities added

--- CampusFinderApp/app/src/main/java/com/upt/ac/campusfinderapp/LoginActivity.java
+++ CampusFinderApp/app/src/main/java/com/upt/ac/campusfinderapp/LoginActivity.java
@@ -1,12 +1,17 @@
 package com.upt.ac.campusfinderapp;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -19,14 +24,30 @@ public class LoginActivity extends AppCompatActivity {
 
     private TextView mTextView;
 
+    private Button logOutButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
         mTextView = findViewById(R.id.textView);
+        logOutButton = findViewById(R.id.logout);
 
         initFirebase();
+
+        logOutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AuthUI.getInstance()
+                        .signOut(LoginActivity.this)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            public void onComplete(@NonNull Task<Void> task) {
+                                // ...
+                            }
+                        });
+            }
+        });
     }
 
     private void initFirebase() {
@@ -60,7 +81,13 @@ public class LoginActivity extends AppCompatActivity {
             if (resultCode == RESULT_OK) {
                 // Successfully signed in
                 FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-                mTextView.setText("Signed in");
+                //mTextView.setText("Signed in");
+
+                Intent intent = new Intent(LoginActivity.this, MenuActivity.class);
+                startActivity(intent);
+
+                //LoginActivity.this.finish();
+
             } else {
                 // Sign in failed. If response is null the user canceled the
                 // sign-in flow using the back button. Otherwise check
