c361f36768839edbeef51b9f3e6df42c9a536c0a
AlexandraAndritoi <alexandraandritoi1@gmail.com>
2019-02-03 11:45:33 +0200

    Outdoor Map implemented with fragment using standard Google Map Activity

--- CampusFinderApp/app/src/main/java/com/upt/ac/campusfinderapp/OutdoorMapFragment.java
+++ CampusFinderApp/app/src/main/java/com/upt/ac/campusfinderapp/OutdoorMapFragment.java
@@ -4,17 +4,35 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
-public class OutdoorMapFragment extends Fragment {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+
+public class OutdoorMapFragment extends Fragment implements OnMapReadyCallback {
+
+    private SupportMapFragment mapFragment;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_outdoor_map, null);
+
+        View v  = inflater.inflate(R.layout.fragment_outdoor_map, null);
+        mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        if(mapFragment == null) {
+            FragmentManager fm = getFragmentManager();
+            FragmentTransaction ft = fm.beginTransaction();
+            mapFragment = SupportMapFragment.newInstance();
+            ft.replace(R.id.map, mapFragment).commit();
+        }
+        mapFragment.getMapAsync(this);
+        return v;
     }
 
     @Override
@@ -22,5 +40,10 @@ public class OutdoorMapFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
         Toast.makeText(getActivity(), "OutdoorMap Fragment", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
     }
 }
