d7cfe6e7d03fe30fa7423c280385ab35864b3212
AlexandraAndritoi <alexandraandritoi1@gmail.com>
2019-02-05 15:10:16 +0200

    Refactoring: created outdoormap package

--- CampusFinderApp/app/src/main/java/com/upt/ac/campusfinderapp/MenuActivity.java
+++ CampusFinderApp/app/src/main/java/com/upt/ac/campusfinderapp/MenuActivity.java
@@ -1,15 +1,11 @@
 package com.upt.ac.campusfinderapp;
 
-import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
-import android.util.AttributeSet;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -20,6 +16,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
+
+import com.upt.ac.campusfinderapp.outdoormap.OutdoorMapFragment;
 
 public class MenuActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
--- CampusFinderApp/app/src/main/java/com/upt/ac/campusfinderapp/outdoormap/OutdoorMapFragment.java
+++ CampusFinderApp/app/src/main/java/com/upt/ac/campusfinderapp/outdoormap/OutdoorMapFragment.java
@@ -1,4 +1,4 @@
-package com.upt.ac.campusfinderapp;
+package com.upt.ac.campusfinderapp.outdoormap;
 
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -14,6 +14,7 @@ import android.widget.Toast;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.upt.ac.campusfinderapp.R;
 
 public class OutdoorMapFragment extends Fragment implements OnMapReadyCallback {
 
