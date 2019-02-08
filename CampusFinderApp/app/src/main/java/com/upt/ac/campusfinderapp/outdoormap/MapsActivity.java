31e011f2218e7dab7b3b228c8f55b01942e4d0c4
AlexandraAndritoi <alexandraandritoi1@gmail.com>
2019-02-08 15:01:22 +0200

    Outdoor Map opens with device location

--- CampusFinderApp/app/src/main/java/com/upt/ac/campusfinderapp/outdoormap/MapsActivity.java
+++ CampusFinderApp/app/src/main/java/com/upt/ac/campusfinderapp/outdoormap/MapsActivity.java
@@ -13,8 +13,6 @@ import com.upt.ac.campusfinderapp.R;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
-    private GoogleMap mMap;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,11 +35,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
--- CampusFinderApp/app/src/main/java/com/upt/ac/campusfinderapp/outdoormap/OutdoorMapFragment.java
+++ CampusFinderApp/app/src/main/java/com/upt/ac/campusfinderapp/outdoormap/OutdoorMapFragment.java
@@ -1,8 +1,12 @@
 package com.upt.ac.campusfinderapp.outdoormap;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
@@ -11,29 +15,118 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.upt.ac.campusfinderapp.R;
 
 public class OutdoorMapFragment extends Fragment implements OnMapReadyCallback {
 
     private SupportMapFragment mapFragment;
+    private GoogleMap mMap;
+
+    private FusedLocationProviderClient fusedLocationClient;
+    private LocationCallback locationCallback;
+    private LocationRequest locationRequest;
+    private boolean requestingLocationUpdates;
+
+    private final String REQUESTING_LOCATION_UPDATES_KEY = "requesting-location-updates-key";
+
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-        View v  = inflater.inflate(R.layout.fragment_outdoor_map, null);
-        mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
-        if(mapFragment == null) {
-            FragmentManager fm = getFragmentManager();
-            FragmentTransaction ft = fm.beginTransaction();
-            mapFragment = SupportMapFragment.newInstance();
-            ft.replace(R.id.map, mapFragment).commit();
+        View v = getViewFromLayout(inflater);
+
+        fusedLocationClient = getLocationProvider();
+        getDeviceLocation();
+        initLocationCallback();
+        createLocationRequest();
+
+        mapFragment = getMapFragmentById();
+        if (mapFragment == null) {
+            createMapFragment();
         }
-        mapFragment.getMapAsync(this);
+        asyncMap();
         return v;
+    }
+
+    private View getViewFromLayout(@NonNull LayoutInflater inflater) {
+        return inflater.inflate(R.layout.fragment_outdoor_map, null);
+    }
+
+    private FusedLocationProviderClient getLocationProvider() {
+        return LocationServices.getFusedLocationProviderClient(this.getActivity());
+    }
+
+    private void getDeviceLocation() {
+        if (ActivityCompat.checkSelfPermission(this.getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this.getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        fusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this.getActivity(), new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        // Got last known location. In some rare situations this can be null.
+                        if (location != null) {
+                            // Logic to handle location object
+                            LatLng locationCoordinates = new LatLng(location.getLatitude(), location.getLongitude());
+                            addMarker(locationCoordinates);
+                            moveCamera(locationCoordinates);
+                        }
+                    }
+                });
+    }
+
+    private void initLocationCallback() {
+        locationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                if (locationResult == null) {
+                    return;
+                }
+                for (Location location : locationResult.getLocations()) {
+                    // Update UI with location data
+                    // ...
+                }
+            }
+
+            ;
+        };
+    }
+
+    private void createLocationRequest() {
+        locationRequest = LocationRequest.create();
+        locationRequest.setInterval(10000);
+        locationRequest.setFastestInterval(5000);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+
+    private SupportMapFragment getMapFragmentById() {
+        return (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+    }
+
+    private void createMapFragment() {
+        FragmentManager fm = getFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        mapFragment = SupportMapFragment.newInstance();
+        ft.replace(R.id.map, mapFragment).commit();
+    }
+
+    private void asyncMap() {
+        mapFragment.getMapAsync(this);
     }
 
     @Override
@@ -44,7 +137,53 @@ public class OutdoorMapFragment extends Fragment implements OnMapReadyCallback {
     }
 
     @Override
-    public void onMapReady(GoogleMap googleMap) {
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        outState.putBoolean(REQUESTING_LOCATION_UPDATES_KEY,
+                requestingLocationUpdates);
+        super.onSaveInstanceState(outState);
+    }
 
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (requestingLocationUpdates) {
+            startLocationUpdates();
+        }
+    }
+
+    private void startLocationUpdates() {
+        if (ActivityCompat.checkSelfPermission(this.getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this.getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, null);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        stopLocationUpdates();
+    }
+
+    private void stopLocationUpdates() {
+        fusedLocationClient.removeLocationUpdates(locationCallback);
+    }
+
+    private void addMarker(LatLng latLng) {
+        mMap.addMarker(new MarkerOptions().position(latLng).title("You're here"));
+    }
+
+    private void moveCamera(LatLng latLng) {
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, 17);
+        mMap.moveCamera(cameraUpdate);
     }
 }
