e2075210d82109ea5c49d4774161e200e3fea847
AlexandraAndritoi <alexandraandritoi1@gmail.com>
2019-02-17 09:58:03 +0200

    Added class for saved places

---
+++ CampusFinderApp/app/src/main/java/com/upt/ac/campusfinderapp/savedplaces/places/SavedPlace.java
@@ -0,0 +1,20 @@
+package com.upt.ac.campusfinderapp.savedplaces.places;
+
+import android.location.Location;
+
+public class SavedPlace {
+    private Location location;
+
+    public SavedPlace(Location location) {
+        this.location = location;
+    }
+
+    public String getLocationName() {
+        // TODO: Get location name
+        return location.toString();
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+}
