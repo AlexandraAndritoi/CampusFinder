f334a1f41034ff9e6147c39c34ce97966feb69e2
AlexandraAndritoi <alexandraandritoi1@gmail.com>
2019-02-19 17:28:20 +0200

    Modified SavedPlace class as a POJO

--- CampusFinderApp/app/src/main/java/com/upt/ac/campusfinderapp/model/SavedPlace.java
+++ CampusFinderApp/app/src/main/java/com/upt/ac/campusfinderapp/model/SavedPlace.java
@@ -1,20 +1,62 @@
 package com.upt.ac.campusfinderapp.model;
 
-import android.location.Location;
+import com.google.android.libraries.places.api.model.Place;
 
 public class SavedPlace {
-    private Location location;
 
-    public SavedPlace(Location location) {
-        this.location = location;
+    private String placeId;
+    private String name;
+    private String address;
+
+    private double latitude;
+    private double longitude;
+
+
+    public SavedPlace(Place place) {
+        placeId = place.getId();
+        name = place.getName();
+        address = place.getAddress();
+        latitude = place.getLatLng().latitude;
+        longitude = place.getLatLng().longitude;
     }
 
-    public String getLocationName() {
-        // TODO: Get location name
-        return location.toString();
+    public String getPlaceId() {
+        return placeId;
     }
 
-    public Location getLocation() {
-        return location;
+    public void setPlaceId(String placeId) {
+        this.placeId = placeId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
     }
 }
