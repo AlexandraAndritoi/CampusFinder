db9a72e16244bad672cc899779b0449006d99b5f
AlexandraAndritoi <alexandraandritoi1@gmail.com>
2019-03-05 18:31:57 +0200

    New Map solution

--- CampusFinderApp/app/src/main/java/com/upt/ac/campusfinderapp/outdoormap/OutdoorMapFragment.java
+++ CampusFinderApp/app/src/main/java/com/upt/ac/campusfinderapp/outdoormap/OutdoorMapFragment.java
@@ -1,274 +1,51 @@
 package com.upt.ac.campusfinderapp.outdoormap;
 
-import android.Manifest;
-import android.app.Activity;
-import android.content.Intent;
-import android.content.IntentSender;
-import android.content.pm.PackageManager;
-import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
-import com.google.android.gms.common.api.ResolvableApiException;
-import com.google.android.gms.common.api.Status;
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationCallback;
-import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.location.LocationResult;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.location.LocationSettingsRequest;
-import com.google.android.gms.location.LocationSettingsResponse;
-import com.google.android.gms.location.SettingsClient;
-import com.google.android.gms.maps.CameraUpdate;
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
-import com.google.android.libraries.places.api.Places;
-import com.google.android.libraries.places.api.model.Place;
-import com.google.android.libraries.places.api.net.PlacesClient;
-import com.google.android.libraries.places.widget.Autocomplete;
-import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
-import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
+import com.tomtom.online.sdk.common.location.LatLng;
+import com.tomtom.online.sdk.map.MapFragment;
+import com.tomtom.online.sdk.map.OnMapReadyCallback;
+import com.tomtom.online.sdk.map.TomtomMap;
+import com.tomtom.online.sdk.map.TomtomMapCallback;
 import com.upt.ac.campusfinderapp.R;
 
-import java.util.Arrays;
-
-public class OutdoorMapFragment extends Fragment implements OnMapReadyCallback {
-
-    private SupportMapFragment mapFragment;
-    private GoogleMap mMap;
-    private AutocompleteSupportFragment autocompleteFragment;
-
-    private FusedLocationProviderClient mFusedLocationClient;
-    private Task<LocationSettingsResponse> mTask;
-    private LocationCallback mLocationCallback;
-    private LocationRequest mLocationRequest;
-    private Location mCurrentLocation;
-
-    private boolean mRequestingLocationUpdates;
-
-    private final String REQUESTING_LOCATION_UPDATES_KEY = "requesting-location-updates-key";
-    private final int REQUEST_CHECK_SETTINGS = 123;
-
-    private Activity menuActivity;
+public class OutdoorMapFragment extends Fragment implements OnMapReadyCallback,
+        TomtomMapCallback.OnMapLongClickListener {
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        menuActivity = this.getActivity();
-
-        updateValuesFromBundle(savedInstanceState);
-        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this.getActivity());
-        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder();
-        SettingsClient client = LocationServices.getSettingsClient(this.getActivity());
-        mTask = client.checkLocationSettings(builder.build());
-        mLocationCallback = new LocationCallback() {
-            @Override
-            public void onLocationResult(LocationResult locationResult) {
-                if (locationResult == null) {
-                    return;
-                }
-                for (Location location : locationResult.getLocations()) {
-                    // Update UI with location data
-                    updateLocationOnScreen(location);
-                }
-            }
-        };
-        mRequestingLocationUpdates = true;
-
-        if(!Places.isInitialized()) {
-            Places.initialize(this.getContext(), "");
-        }
-//        PlacesClient placesClient = Places.createClient(this.getContext());
-    }
-
-    private void updateValuesFromBundle(Bundle savedInstanceState) {
-        if (savedInstanceState == null) {
-            return;
-        }
-        // Update the value of mRequestingLocationUpdates from the Bundle.
-        if (savedInstanceState.keySet().contains(REQUESTING_LOCATION_UPDATES_KEY)) {
-            mRequestingLocationUpdates = savedInstanceState.getBoolean(
-                    REQUESTING_LOCATION_UPDATES_KEY);
-        } // Update UI to match restored state
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        if (mRequestingLocationUpdates) {
-            startLocationUpdates();
-        }
-    }
-
-    private void startLocationUpdates() {
-        checkPermission();
-        mFusedLocationClient.requestLocationUpdates(mLocationRequest, mLocationCallback, null);
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-        stopLocationUpdates();
-    }
-
-    private void stopLocationUpdates() {
-        mFusedLocationClient.removeLocationUpdates(mLocationCallback);
+        Toast.makeText(getActivity(), "OutdoorMap Fragment", Toast.LENGTH_SHORT).show();
     }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-
         View v = inflater.inflate(R.layout.fragment_outdoor_map, null);
-
-
-        autocompleteFragment = (AutocompleteSupportFragment)
-                getChildFragmentManager().findFragmentById(R.id.autocomplete_fragment);
-        autocompleteFragment.setPlaceFields(Arrays.asList(Place.Field.ID, Place.Field.NAME, Place.Field.LAT_LNG, Place.Field.ADDRESS));
-        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
-            @Override
-            public void onPlaceSelected(Place place) {
-                // TODO: Get info about the selected place.
-                addMarker(place.getLatLng());
-            }
-
-            @Override
-            public void onError(Status status) {
-                // TODO: Handle the error.
-            }
-        });
-        createLocationRequest();
-        mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
-        if (mapFragment == null) {
-            createMapFragment();
-        }
-        mapFragment.getMapAsync(this);
+        initTomTomServices();
+        initUIViews();
+        setupUIViewListeners();
         return v;
     }
 
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-
-        checkPermission();
-        mFusedLocationClient.getLastLocation()
-                .addOnSuccessListener(this.getActivity(), new OnSuccessListener<Location>() {
-                    @Override
-                    public void onSuccess(Location location) {
-                        // Got last known location. In some rare situations this can be null.
-                        if (location != null) {
-                            // Logic to handle location object
-                            updateLocationOnScreen(location);
-                        }
-                    }
-                });
-
-        mTask.addOnSuccessListener(this.getActivity(), new OnSuccessListener<LocationSettingsResponse>() {
-            @Override
-            public void onSuccess(LocationSettingsResponse locationSettingsResponse) {
-                // All location settings are satisfied. The client can initialize
-                // location requests here.
-                // ...
-            }
-        });
-
-        mTask.addOnFailureListener(this.getActivity(), new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                if (e instanceof ResolvableApiException) {
-                    // Location settings are not satisfied, but this can be fixed
-                    // by showing the user a dialog.
-                    try {
-                        // Show the dialog by calling startResolutionForResult(),
-                        // and check the result in onActivityResult().
-                        ResolvableApiException resolvable = (ResolvableApiException) e;
-                        resolvable.startResolutionForResult(menuActivity,
-                                REQUEST_CHECK_SETTINGS);
-                    } catch (IntentSender.SendIntentException sendEx) {
-                        // Ignore the error.
-                    }
-                }
-            }
-        });
+    private void initTomTomServices() {
+        MapFragment mapFragment = (MapFragment) getChildFragmentManager().findFragmentById(R.id.tomtom_map);
+        mapFragment.getAsyncMap(this);
     }
 
-    private void createLocationRequest() {
-        mLocationRequest = LocationRequest.create();
-        mLocationRequest.setInterval(10000);
-        mLocationRequest.setFastestInterval(2000);
-        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-    }
-
-    private void createMapFragment() {
-        FragmentManager fm = getFragmentManager();
-        FragmentTransaction ft = fm.beginTransaction();
-        mapFragment = SupportMapFragment.newInstance();
-        ft.replace(R.id.map, mapFragment).commit();
-    }
+    private void initUIViews() {}
+    private void setupUIViewListeners() {}
 
     @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-
-        Toast.makeText(getActivity(), "OutdoorMap Fragment", Toast.LENGTH_SHORT).show();
-    }
+    public void onMapReady(@NonNull TomtomMap tomtomMap) {}
 
     @Override
-    public void onSaveInstanceState(@NonNull Bundle outState) {
-        outState.putBoolean(REQUESTING_LOCATION_UPDATES_KEY,
-                mRequestingLocationUpdates);
-        super.onSaveInstanceState(outState);
-    }
-
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-    }
-
-    private void addMarker(LatLng latLng) {
-        mMap.addMarker(new MarkerOptions().position(latLng).title("You're here"));
-    }
-
-    private void updateLocationOnScreen(Location location) {
-        mCurrentLocation = location;
-        LatLng coordinates = new LatLng(mCurrentLocation.getLatitude(), mCurrentLocation.getLongitude());
-        moveCamera(coordinates);
-    }
-
-    private void moveCamera(LatLng latLng) {
-        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, 17);
-        mMap.moveCamera(cameraUpdate);
-        checkPermission();
-        mMap.setMyLocationEnabled(true);
-    }
-
-    private void checkPermission() {
-        if (ActivityCompat.checkSelfPermission(this.getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
-                && ActivityCompat.checkSelfPermission(this.getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            return;
-        }
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-    }
+    public void onMapLongClick(@NonNull LatLng latLng) {}
 }
